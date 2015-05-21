8cd9784daa75df01ade39904a5dc7d3a51c5a379
Mariya <mbeketova@rambler.ru>
2015-05-21 17:43:21 +0300

    Initial Commit

---
+++ SearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchBar
+//
+//  Created by Admin on 21.05.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
